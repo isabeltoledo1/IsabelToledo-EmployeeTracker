c3bb70ff588e29f3277d2f38fe1f7d80022d2d84
Isabel Toledo <isabeltoledo9@gmail.com>
2021-04-26 16:43:11 -0600

    added starter function

--- index.js
+++ index.js
@@ -21,5 +21,64 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err
   console.log("Connected as Id" + connection.threadId)
-  startPrompt();
+  start();
 });
+
+
+function start() {
+  inquirer.prompt([
+  {
+  type: "list",
+  message: "What would you like to do?",
+  name: "choice",
+  choices: [
+            "View All Employees?", 
+            "View All Employee's By Roles?",
+            "View all Emplyees By Deparments", 
+            "Update Employee",
+            "Add Employee?",
+            "Add Role?",
+            "Add Department?"
+          ]
+  }
+]).then(function(val) {
+      switch (val.choice) {
+          case "View All Employees?":
+            viewAllEmployees();
+          break;
+  
+        case "View All Employee's By Roles?":
+            viewAllRoles();
+          break;
+        case "View all Emplyees By Deparments":
+            viewAllDepartments();
+          break;
+        
+        case "Add Employee?":
+              addEmployee();
+            break;
+
+        case "Update Employee":
+              updateEmployee();
+            break;
+    
+          case "Add Role?":
+              addRole();
+            break;
+    
+          case "Add Department?":
+              addDepartment();
+            break;
+  
+          }
+  })
+}
+
+function viewAllEmployees() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+  function(err, res) {
+    if (err) throw err
+    console.table(res)
+    start()
+})
+}
