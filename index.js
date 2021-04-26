73eb13212e78440be75c7749f51ca1a3fb85710d
Isabel Toledo <isabeltoledo9@gmail.com>
2021-04-26 16:44:39 -0600

    added function for roles

--- index.js
+++ index.js
@@ -82,3 +82,12 @@ function viewAllEmployees() {
     start()
 })
 }
+
+function viewAllRoles() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+  function(err, res) {
+  if (err) throw err
+  console.table(res)
+  start()
+  })
+}
